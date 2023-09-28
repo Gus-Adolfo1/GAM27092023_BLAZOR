46874d97f98e5462b370fc41a452ab655d7a06b4
Gustavo Munoz <softwareadolfo@gmail.com>
2023-09-27 18:38:33 -0600

    Gustavo agregando paquetes nugets y entidades

---
+++ GAM27092023_BLAZOR/EN/TableGAM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace GAM27092023_BLAZOR.EN
+{
+    public class TableGAM
+    {
+        public int id { get; set; }
+        
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public decimal height { get; set; }
+
+        public DateTime dob { get; set; }
+
+    }
+}
